f1ddb217d113c741a95c130fa8c72799f2876adf
Łukasz Bielenin <l.bielenin@gmail.com>
2018-05-15 22:27:32 +0200

    Implement recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe('Great Recipe', 'I dunno, but this is great',
+    'https://s.hdnux.com/photos/66/62/62/14363832/3/920x920.jpg'),
+    new Recipe('Another magnificent recipe!', 'Still great',
+    'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Dog-bite-prevention.jpg?itok=3N2KTNkP')
+  ];
+
   constructor() { }
 
   ngOnInit() {
